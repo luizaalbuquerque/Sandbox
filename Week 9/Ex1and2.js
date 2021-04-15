24c2fbc93ca11d2dd176f83f0cd558f04cca308c
snelbops <jsnel@cct.ie>
2021-04-15 10:30:05 +0100

    2nd commit

---
+++ Week 9/Ex1and2.js
@@ -0,0 +1,16 @@
+// Exercise 1
+const sayHello = function () {
+	console.log('hello');
+}
+// Arrow function
+const sayHello = () => console.log('hello');
+
+
+// Exercise 2
+const add  = function (x, y) {
+    retunr = x + y;
+    return sum;
+}
+// Arrow function
+const add = (x, y) => x + y;
+
---
+++ Week 9/arrow.js
@@ -0,0 +1,23 @@
+function myFunction(arg1, arg2){
+    // function body
+}
+
+const myOtherFunction = function (arg1, arg2){
+    // function body
+}
+
+// -----------
+const add = function(x, y){
+    const sum = x + y;
+    return sum;
+}
+
+const add = (x, y) => x + y;
+
+// -----------
+const double = function(number) {
+    return number*2;
+}
+
+const double = number => number * 2;
+
